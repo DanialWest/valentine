8ac3d2ee3ab7c8959ed669756dfaf807313e023e
DanialWest <159823764+DanialWest@users.noreply.github.com>
2024-02-13 01:30:15 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.json());
+//const mongoose = require('mongoose');
+
+
+
+ 
 
 app.use(express.static(path.join(__dirname, 'valentine.github.io-main')));
 
-app.post("/foods", (req, res) => {
+app.post("/foods", async (req, res) => {
     console.log(req.body);          // this would be the data sent with the request
-
+   
 });
 
-app.post("/desserts", (req, res) => {
+app.post("/desserts", async (req, res) => {
     console.log(req.body);          // this would be the data sent with the request
-
+ 
 });
 
-app.post("/activities", (req, res) => {
+app.post("/activities", async (req, res) => {
     console.log(req.body);          // this would be the data sent with the request
-
+   
 });
+
+
+  
 
 app.listen(3000, () => {
     console.log("App working BLYa");
